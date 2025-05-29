dc91b90104ed8c885092ee614339437ae6dac7d3
msyb <msyb@itu.dk>
2025-05-29 21:39:05 +0200

    does this the test run

--- internal/types/ingredient.go
+++ internal/types/ingredient.go
@@ -1,7 +1,7 @@
 package types
 
 type Ingredient struct {
-	ID       string  `json:id`
-	Name     string  `json:"name"`
-	Unit     string  `json:"unit"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Unit string `json:"unit"`
 }
